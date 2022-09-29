1bb6b00c4360cfccf0ffd2669a1ce8d7b3c445a3
Marc Parra <marcparra1307@gmail.com>
2022-09-29 10:13:50 +0200

    TA18 - PartsSuppliers

--- TA18 - PartsSuppliersBD/src/PartsSuppliers.java
+++ TA18 - PartsSuppliersBD/src/PartsSuppliers.java
@@ -14,7 +14,9 @@ public class PartsSuppliers {
 		try { 
 			//Class.forName("com.mysql.jdbc.Driver"); 
 			//conn = DriverManager.getConnection(url, login, password); 
-			String sURL = url; conn = DriverManager.getConnection(sURL,login,password); 
+			String sURL = url; 
+			conn = DriverManager.getConnection(sURL,login,password); 
+			
 			if (conn != null) { 
 				System.out.println("-Abierta base de datos " + url + " - Ok"); 
 			// Crear tabla contacto 
